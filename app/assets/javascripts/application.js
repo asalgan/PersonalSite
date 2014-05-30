234393a1921dd37e97ae2ee9f35b440e65855b3f
Alan Salganik <asalganik1@gmail.com>
2014-05-29 22:44:39 -0500

    javascript changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,28 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+$(document).ready(function() { 
+
+	$.fn.followTo = function (pos) {
+    var $this = this,
+      $window = $(window);
+
+  $window.scroll(function (e) {
+    if ($window.scrollTop() > pos) {
+      $this.css({
+          position: 'absolute',
+          top: pos
+       });
+    } else {
+        $this.css({
+          position: 'fixed',
+          top: 0
+        });
+      }
+    });
+	};
+
+	$('.name').followTo(710);
+
+});
